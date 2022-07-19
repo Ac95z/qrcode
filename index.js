7e5220936cd068adcf6d46ca79abb2f82060f797
Kqin <97442619+Kqin96@users.noreply.github.com>
2022-07-19 16:45:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_VOICE_STATES",],
 });
 
-client.on('ready', () => {
-  client.user.setActivity('タイトル', { status: "online" });
-  console.log(`${client.user.tag}の起動完了`);
-  console.log(`${client.guilds.cache.size}個のサーバーで稼働中`);
-  console.log(`${client.users.cache.size}人が参加中です。`);
+const QRCode = require("qrcode");
+client.on("messageCreate", async message => {
+  if (message.author.bot) return;
+  if (message.content.startsWith("!qr")) {
+    const qr = message.content.split(" ")[1]
+    if (!qr) return message.reply("キーワードを入力してください");
+    QRCode.toFile("qr.png", `${qr}`);
+    message.reply({ files: ["./qr.png"] });
+  };
 });
-
-
-const QRCode = require('qrcode');
-client.on('messageCreate', async message => {
-  if (message.content.startsWith('!qrcode')) {
-    QRCode.toFile('qr.png', `${message.content.split(' ')[1]}`);
-    message.reply({ files: ['./qr.png'] })
-  }
-})
 
 client.login(process.env.TOKEN);
