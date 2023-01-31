484f3902534f4b8dc58bf653190a5dc0ac79f1b8
Nanogy <97442619+Nanogy98@users.noreply.github.com>
2023-01-31 14:53:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const QRCode = require("qrcode");
 client.on("messageCreate", async message => {
   if (message.author.bot) return;
   if (message.content.startsWith("!qr")) {
-    const qr = message.content.split(" ").slice(1).join(" ")
+    const qr = message.content.split(" ");
     if (!qr) return message.reply("キーワードを入力してください");
     QRCode.toFile("qr.png", `${qr}`);
     message.reply({ files: ["./qr.png"] });
